194f0f6f990ffed8684b21417844e95ddbe0e18a
Zena Jun <zena.jun@gmail.com>
2018-06-01 16:27:28 -0400

    Working Hamburger menu

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,20 +1,6 @@
-// function myFunction() {
-//   var x = document.getElementById("myTopnav");
-//   if (x.className === "topnav") {
-//     x.className += " responsive";
-//   } else {
-//     x.className = "topnav";
-//   }
-// }
-
-const hamburger = () => {
-  const nav = document.getElementById('headerNav');
-  console.log('hamburger clicked');
-  
-  // if (nav.className === '') {
-  //   nav.className += ' responsive';
-  // } else {
-  //   nav.className = '';
-  // }
-
-}
+$(function() {
+  // Hamburger menu
+  $('.menu').click(() => {
+    $('ul').toggleClass('active');    
+  });  // Hamburger menu end
+});
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,21 +1,8 @@
 'use strict';
 
-// function myFunction() {
-//   var x = document.getElementById("myTopnav");
-//   if (x.className === "topnav") {
-//     x.className += " responsive";
-//   } else {
-//     x.className = "topnav";
-//   }
-// }
-
-var hamburger = function hamburger() {
-  var nav = document.getElementById('headerNav');
-  console.log('hamburger clicked');
-
-  // if (nav.className === '') {
-  //   nav.className += ' responsive';
-  // } else {
-  //   nav.className = '';
-  // }
-};
+$(function () {
+  // Hamburger menu
+  $('.menu').click(function () {
+    $('ul').toggleClass('active');
+  }); // Hamburger menu end
+});
